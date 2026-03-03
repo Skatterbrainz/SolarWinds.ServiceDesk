19ff1f7e814c359e667586e85862cdc6f56cf4a2
skatterbrainz <dstein2017@gmail.com>
2026-03-03 16:04:54 -0500

    Refactor Get-SwSdSite function to improve parameter handling and API data retrieval

--- public/Get-SwSdSite.ps1
+++ public/Get-SwSdSite.ps1
@@ -6,12 +6,14 @@ function Get-SwSdSite {
 		Returns the Service Desk site records for the specified ID or all sites.
 	.PARAMETER Name
 		The site name or ID. If provided, returns the specific site record.
+	.PARAMETER Id
+		The site ID. If provided, returns the specific site record.
 	.EXAMPLE
 		Get-SwSdSite -Name "Main Office"
 		
 		Returns the site record for the specified name.
 	.EXAMPLE
-		Get-SwSdSite -Name "12345"
+		Get-SwSdSite -Id "1234567"
 
 		Returns the site record for the specified ID.
 	.EXAMPLE
@@ -25,18 +27,15 @@ function Get-SwSdSite {
 	[OutputType([PSCustomObject])]
 	[Alias('Get-SwSdSites', 'Get-SwSdSiteList')]
 	param(
-		[parameter(Mandatory = $False)][string]$Name
+		[parameter(Mandatory = $False)][string]$Name,
+		[parameter(Mandatory = $False)][string]$Id
 	)
 	try {
-		$sites = getApiResponse -ApiName "Other Assets List"
-		if ($sites) {
-			if (![string]::IsNullOrWhiteSpace($Name)) {
-				$sites | Where-Object { $_.name -eq $Name -or $_.id -eq $Name}
-			} else {
-				return $sites
-			}
+		$sites = getApiListOrItem -ApiName "Sites List" -Id $Id -PerPage 100
+		if (![string]::IsNullOrEmpty($Name)) {
+			$sites | Where-Object { $_.name -eq $Name }
 		} else {
-			throw "Failed to retrieve sites. Status code: $($response.StatusCode)"
+			$sites
 		}
 	} catch {
 		Write-Error $_.Exception.Message
